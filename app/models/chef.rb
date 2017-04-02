907b74666dfefb554597dbf7c6f91da4c0989311
Serge Lafontaine <slafontaine10@gmail.com>
2017-04-02 13:26:22 -0400

    add chef and recipe model validation and tests

---
+++ app/models/chef.rb
@@ -0,0 +1,11 @@
+class Chef < ApplicationRecord
+  before_save { self.email = email.downcase }
+
+  validates :chefname, presence: true, length: {maximum: 30}
+  VALID_EMAIL_REGEX = /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :email, presence:true, length: {maximum: 255}, 
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false}
+
+
+end
